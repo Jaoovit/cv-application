6436ccb191e0a65660bc6191b4242e4bc5a2f3cc
João Oliveira <jaoovit@gmail.com>
2024-06-24 15:11:36 +0100

    add a icon, change the page title and add a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+    <div className="py-8 flex justify-center text-xl">
+        <footer>Copyright &copy;
+            <a href="https://github.com/Jaoovit">João Vitor de Oliveira <i class='fa fa-github'></i></a>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Curriculum from './components/Curriculum'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Curriculum />
+    <Footer />
   </React.StrictMode>,
 )
